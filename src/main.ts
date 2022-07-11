033b8c5f08da42e2ab4bb7e11f0572177c1aac2f
Brando Juberd <brandojuberd@gmail.com>
2022-07-11 21:02:03 +0700

    change on src/

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 8080);
   console.log("This change should reflect on workflows")
+  console.log("Change number: ", 1)
   console.log(`Application is running on: ${await app.getUrl()}`);
 
 }
