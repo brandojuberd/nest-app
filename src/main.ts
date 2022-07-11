5b368c02fd038ee491a197e64c2d949a68637979
Brando Juberd <brandojuberd@gmail.com>
2022-07-11 23:09:18 +0700

    add change to check pm2 deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 8080);
   console.log("This change should reflect on workflows")
-  console.log("Change number: ", 1)
+  console.log("Change number: ", 2)
   console.log(`Application is running on: ${await app.getUrl()}`);
 
 }
